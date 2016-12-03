534ef1197f749cdbf90766814f447b434e2b8238
singtolee <liqiang0618@outlook.com>
2016-12-03 10:50:03 +0800

    Initial Commit

---
+++ SingtoStore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SingtoStore
+//
+//  Created by li qiang on 12/3/2559 BE.
+//  Copyright © 2559 Singto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
